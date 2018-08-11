9caa7b2eb9c42830124ddfa3f9a6373b8baeae48
Kevin Stadler <kevin.stadler@ed.ac.uk>
2018-08-11 21:15:31 +0200

    Update AudioIn javadocs and add more AudioIn.play() signatures

--- src/processing/sound/AudioIn.java
+++ src/processing/sound/AudioIn.java
@@ -9,10 +9,6 @@ import processing.core.PApplet;
  * AudioIn lets you grab the audio input from your sound card.
  * 
  * @webref sound
- * @param parent
- *            PApplet: typically use "this"
- * @param in
- *            input channel number (optional, default 0)
  **/
 public class AudioIn extends SoundObject {
 
@@ -27,6 +23,12 @@ public class AudioIn extends SoundObject {
 		this(parent, 0);
 	}
 
+	/**
+	 * @param parent
+	 *            typically use "this"
+	 * @param in
+	 *            input channel number (optional, default 0)
+	 */
 	public AudioIn(PApplet parent, int in) {
 		super(parent);
 		this.input = new ChannelIn(in);
@@ -40,21 +42,38 @@ public class AudioIn extends SoundObject {
 		this.circuit.add(this.input);
 	}
 
-	/**
-	 * Start capturing the input stream and route it to the audio output
-	 * 
-	 * @webref sound
-	 **/
 	public void play() {
 		super.play();
 	}
 
+	public void play(float amp) {
+		this.amp(amp);
+		this.play();
+	}
+
+	public void play(float amp, float add) {
+		this.add(add);
+		this.play(amp);
+	}
+
 	/**
-	 * Start the input stream without routing it to the audio output. This is useful
-	 * if you only want to perform audio analysis based on the microphone input.
-	 * 
+	 * Start capturing the input stream and route it to the audio output
+	 *
+	 * @param amp
+	 *            the volume to grab the input at as a value from 0.0 (complete
+	 *            silence) to 1.0 (full volume)
+	 * @param add
+	 *            offset the audio input by the given value
+	 * @param pos
+	 *            pan the audio input in a stereo panorama. Allowed values are
+	 *            between -1.0 (left) and 1.0 (right)
 	 * @webref sound
-	 */
+	 **/
+	public void play(float amp, float add, float pos) {
+		this.set(amp, add, pos);
+		this.play();
+	}
+
 	public void start() {
 		Engine.getEngine().add(this.circuit);
 	}
@@ -69,6 +88,20 @@ public class AudioIn extends SoundObject {
 		this.start(amp);
 	}
 
+	/**
+	 * Start the input stream without routing it to the audio output. This is useful
+	 * if you only want to perform audio analysis based on the microphone input.
+	 *
+	 * @param amp
+	 *            the volume to grab the input at as a value from 0.0 (complete
+	 *            silence) to 1.0 (full volume)
+	 * @param add
+	 *            offset the audio input by the given value
+	 * @param pos
+	 *            pan the audio input in a stereo panorama. Allowed values are
+	 *            between -1.0 (left) and 1.0 (right)
+	 * @webref sound
+	 */
 	public void start(float amp, float add, float pos) {
 		this.set(amp, add, pos);
 		this.start();
@@ -79,12 +112,13 @@ public class AudioIn extends SoundObject {
 	 * 
 	 * @webref sound
 	 * @param amp
-	 *            Amplitude value between 0.0 and 1.0
+	 *            the volume to grab the input at as a value from 0.0 (complete
+	 *            silence) to 1.0 (full volume)
 	 * @param add
-	 *            Offset the generator by a given value
+	 *            offset the audio input by the given value
 	 * @param pos
-	 *            Pan the generator in stereo panorama. Allowed values are between
-	 *            -1.0 and 1.0.
+	 *            pan the audio input in a stereo panorama. Allowed values are
+	 *            between -1.0 (left) and 1.0 (right)
 	 **/
 	public void set(float amp, float add, float pos) {
 		this.amp(amp);
